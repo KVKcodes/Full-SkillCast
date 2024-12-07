cf39cc934c1c3dcc9e84bf3bf5c847c1535a041b
KVK Siddartha <sidduultralegend@gmail.com>
2024-12-07 21:39:29 +0530

    ev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('./certs/key.pem'),
-      cert: fs.readFileSync('./certs/cert.pem'),
-    },
+    // https: {
+    //   key: fs.readFileSync('./certs/key.pem'),
+    //   cert: fs.readFileSync('./certs/cert.pem'),
+    // },
     port: 5173
   }
 });
